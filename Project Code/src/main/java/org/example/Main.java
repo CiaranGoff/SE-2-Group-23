7c34c37175cb4108b4243d43692db2ab640321fc
markmcp123 <mark.mcphillips@ucdconnect.ie>
2026-02-17 12:32:47 +0000

    Moved over ProjectCode/src/main code into Project Code(Maven folder)

--- Project Code/src/main/java/org/example/Main.java
+++ Project Code/src/main/java/org/example/Main.java
@@ -1,22 +1,33 @@
 package org.example;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Hello JavaFX!");
-        Scene scene = new Scene(label, 400, 200);
+    public void start(Stage stage) throws Exception {
 
-        stage.setTitle("Test");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/SelectGameMode.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Quax");
         stage.setScene(scene);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
