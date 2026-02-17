0cc86639b5bb91b253bd21b3c861348d1fa6bba8
Ciaran <ciaran.goff1@ucdconnect.ie>
2026-02-17 10:11:42 +0000

    SR1 Completed. Display a pane giving an option to choose human or bot mode.

---
+++ ProjectCode/src/main/java/org/example/GameModeController.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class GameModeController {
+
+    @FXML
+    private void handleHuman(ActionEvent event) throws Exception {
+        loadBoard(event, "HUMAN");
+    }
+
+    @FXML
+    private void handleBot(ActionEvent event) throws Exception {
+        loadBoard(event, "BOT");
+    }
+
+    private void loadBoard(ActionEvent event, String mode) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/QuaxBoard.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(root, 800, 600));
+        stage.setTitle("Quax - " + mode);
+        stage.show();
+    }
+}
---
+++ ProjectCode/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/SelectGameMode.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Quax");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
