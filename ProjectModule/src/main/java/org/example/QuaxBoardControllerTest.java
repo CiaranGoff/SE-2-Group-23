b93ddd1fc98b422a1bb02e34ee5b8b222f65b0de
markmcp123 <mark.mcphillips@ucdconnect.ie>
2026-02-24 10:38:24 +0000

    Updated the QuaxBoardControllerTest.java from the QuaxBoardControllerTest.class.

--- ProjectModule/src/main/java/org/example/QuaxBoardControllerTest.java
+++ ProjectModule/src/main/java/org/example/QuaxBoardControllerTest.java
@@ -1,24 +1,44 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package org.example;
 
+import java.lang.reflect.Field;
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class QuaxBoardControllerTest {
-
     @Test
-    public void setModeHumanTest(){
+    public void handleHumanTest() {
         QuaxBoardController controller = new QuaxBoardController();
         controller.setMode("HUMAN");
 
-        assertEquals("HUMAN", controller.getMode());
+        try {
+            Field field = QuaxBoardController.class.getDeclaredField("mode");
+            field.setAccessible(true);
+            String modeValue = (String)field.get(controller);
+            Assert.assertEquals("HUMAN", modeValue);
+        } catch (Exception var4) {
+            System.out.println("Failed to access the private mode field to set mode to Human");
+        }
+
     }
 
     @Test
-    public void setModeBotTest(){
+    public void handleBotTest() {
         QuaxBoardController controller = new QuaxBoardController();
         controller.setMode("BOT");
 
-       assertEquals("BOT", controller.getMode());
-    }
+        try {
+            Field field = QuaxBoardController.class.getDeclaredField("mode");
+            field.setAccessible(true);
+            String modeValue = (String)field.get(controller);
+            Assert.assertEquals("BOT", modeValue);
+        } catch (Exception var4) {
+            System.out.println("Failed to access the private mode field to set mode to Bot");
+        }
 
+    }
 }
