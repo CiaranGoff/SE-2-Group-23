a2598042f67554a9ee6efb1e74a8662d3298da32
kianmcgee05 <kian.mcgee@ucdconnect.ie>
2026-03-04 13:43:07 +0000

    Added additional test cases for sprint 2 implementations.Simplified code in previous completed tests and commented on all tests

--- ProjectModule/src/main/java/org/example/QuaxBoardControllerTest.java
+++ ProjectModule/src/main/java/org/example/QuaxBoardControllerTest.java
@@ -9,36 +9,81 @@ import java.lang.reflect.Field;
 import org.junit.Assert;
 import org.junit.Test;
 
+/* Unit tests for the QuaxBoardController Class, which contains main and the basic
+ * logic functions that determine how a game progresses, such as the getter and
+ * setter methods, and ensuring players take their turns in the right order.
+ * Note: This test class does not test the JavaFX side of the game, as we are only
+ * looking at testing the controller logic.
+ */
 public class QuaxBoardControllerTest {
+
+    /* Test to ensure the setMode() and getMode() functions works for HUMAN mode
+     * Use of assertEquals to show functions work correctly and show same mode
+     */
     @Test
     public void handleHumanTest() {
         QuaxBoardController controller = new QuaxBoardController();
         controller.setMode("HUMAN");
 
-        try {
-            Field field = QuaxBoardController.class.getDeclaredField("mode");
-            field.setAccessible(true);
-            String modeValue = (String)field.get(controller);
-            Assert.assertEquals("HUMAN", modeValue);
-        } catch (Exception var4) {
-            System.out.println("Failed to access the private mode field to set mode to Human");
-        }
-
+        Assert.assertEquals("HUMAN", controller.getMode());
     }
 
+    /* Test to ensure the setMode() and getMode() functions works for BOT mode
+     * Use of assertEquals to show functions work correctly and show same mode
+     */
     @Test
     public void handleBotTest() {
         QuaxBoardController controller = new QuaxBoardController();
         controller.setMode("BOT");
 
-        try {
-            Field field = QuaxBoardController.class.getDeclaredField("mode");
-            field.setAccessible(true);
-            String modeValue = (String)field.get(controller);
-            Assert.assertEquals("BOT", modeValue);
-        } catch (Exception var4) {
-            System.out.println("Failed to access the private mode field to set mode to Bot");
-        }
+        Assert.assertEquals("BOT", controller.getMode());
+    }
 
+    /* Test to show that when a new game is created, that player BLACK will go first
+     * Use of a field to set it to what the boolean value of "blackTurn" is in QuaxBoardController
+     * assertTrue to show that value for "blackTurn" is true when a new game is created
+     */
+    @Test
+    public void blackPlayerGoesFirstTest() throws Exception {
+        QuaxBoardController controller = new QuaxBoardController();
+
+        Field field = QuaxBoardController.class.getDeclaredField("blackTurn");
+        field.setAccessible(true);
+
+        boolean value = (boolean) field.get(controller);
+        Assert.assertTrue(value);
+    }
+
+    /* Test to show that when a new game is created, that the pie rule will appear after BLACK'S turn
+     * Use of a field to set it to what the boolean value of "pieRuleAvailable" is in QuaxBoardController
+     * assertTrue to show that value for "pieRuleAvailable" is true when a new game is created
+     */
+    @Test
+    public void pieRuleAppearsTest() throws Exception{
+        QuaxBoardController controller = new QuaxBoardController();
+
+        Field field = QuaxBoardController.class.getDeclaredField("pieRuleAvailable");
+        field.setAccessible(true);
+
+        boolean value = (boolean) field.get(controller);
+        Assert.assertTrue(value);
+    }
+
+    /* Test to show that when a new game is created, that whatever tile player BLACK chooses
+     * for their first move, that it is null. This highlights that BLACK can pick from any tiles
+     * when a new game is created.
+     * Use of a field to set it to what the Object value of "blackFirstMove" is in QuaxBoardController
+     * assertNull to show that value for "BlackFirstMove" is null when a new game is created, and that BLACK
+     * can choose any tile when they start a game
+     */
+    @Test
+    public void blackFirstMoveIsNullTest() throws Exception{
+        QuaxBoardController controller = new QuaxBoardController();
+
+        Field field = QuaxBoardController.class.getDeclaredField("blackFirstMove");
+        field.setAccessible(true);
+
+        Object value = field.get(controller);
+        Assert.assertNull(value);
     }
 }
